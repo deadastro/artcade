236d79b4b418780700ec4d1b24f14df9e764621f
deadastro <39497624+deadastro@users.noreply.github.com>
2018-06-03 23:18:44 +0300

    v 0.2
    Minor updates.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+var q = 0,
+    w = 0;
+
 class Player {
     
     constructor() {
@@ -14,7 +17,7 @@ class Player {
             'a-1.png',
         ];    
         
-        this.object = $('#player');
+        this.object = $('#player-1');
         this.jumpFrame(0);
     }
     
@@ -36,10 +39,50 @@ class Player {
     }
 }
 
+class Enemy {
+    
+    constructor() {
+        this.framesPerSecond = 30;
+        this.frames = [
+            'd-1.png',
+            'd-2.png',
+            'd-3.png',
+            'd-4.png',
+            'd-4.png',
+            'd-3.png',
+            'd-2.png',
+            'd-1.png',
+        ];    
+        
+        this.object = $('#player-2');
+        this.jumpFrame(0);
+    }
+    
+    jump() {
+        let i = 1;
+        this.jumpFrame(i);
+        
+        let intervalId = setInterval(() => {
+            i++;
+            this.jumpFrame(i);
+            if (i === this.frames.length - 1) {
+                clearInterval(intervalId);
+            }
+        }, 1000 / this.framesPerSecond);
+    }
+    
+    jumpFrame(frame) {
+        this.object.css('background-image', 'url(public/img/' + this.frames[frame] + ')');
+    }
+}
+
+
 $(function() {
     let s = 0,
         l = 0,
-        artyom = new Player();
+        v = 0,
+        artyom = new Player(),
+        ded = new Enemy();
     
     $(document).on('keyup', (e) => {
         if (e.which === 32) {
@@ -59,9 +102,48 @@ $(function() {
                 }, 3000);
                 $('#level span').text(l)
             }
+            q = s;
+        }
+        
+    });
+    
+        $(document).on('keyup', (e) => {
+        if (e.which === 38) {
+            ded.jump();
+            v++;
+            $('#score-e').html('Score: ' + v);
+            if (v % 10 === 0) {
+                l++;
+                $('.level-popup').addClass('active').find('span').text(l);
+                $('.level-popup-shade').addClass('active');
+                $('#hp').css('width', l*10);
+                $('#speed').css('width', l*10);
+                $('#ko h1').append('<i class="fas fa-bolt"></i>')
+                setTimeout(() => {
+                    $('.level-popup').removeClass('active');
+                    $('.level-popup-shade').removeClass('active');
+                }, 3000);
+                $('#level span').text(l)
+            }
+            w = v;
         }
         
     });
 });
 
-
+function timer(){
+ var obj = $('#timer')[0];
+ obj.innerHTML--;
+  
+ if (obj.innerHTML == 0) {
+     $('body').empty();
+     $('body').append('<div id="result"></div>')
+     $('body #result').append('<h1>Счет: ' + q + '</h1>');
+     $('body #result').append('<h1>Счет: ' + w + '</h1>');
+     setTimeout(function(){},1000);
+ }
+ else {
+     setTimeout(timer,1000);
+ }
+}
+setTimeout(timer,1000);
